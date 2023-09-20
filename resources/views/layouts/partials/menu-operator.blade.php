9b9ab97de0532c07024d1ed262abf7c2d5978457
alfin59173 <alfin59173@gmail.com>
2023-09-20 22:10:47 +0700

    koment

--- resources/views/layouts/partials/menu-operator.blade.php
+++ resources/views/layouts/partials/menu-operator.blade.php
@@ -1,21 +1,19 @@
 {{-- <li class="sidebar-item">
     <a class="sidebar-link {{ request()->routeIs('operator-downtime.*') ? 'active' : '' }}"
-        href="{{ route('operator-downtime.index') }}"
-        aria-expanded="false">
-        <span>
-            <i class="ti ti-clock-stop"></i>
-        </span>
-        <span class="hide-menu">Input DownTime</span>
-    </a>
+href="{{ route('operator-downtime.index') }}"
+aria-expanded="false">
+<span>
+    <i class="ti ti-clock-stop"></i>
+</span>
+<span class="hide-menu">Input DownTime</span>
+</a>
 </li> --}}
 {{-- <li class="nav-small-cap">
     <i class="ti ti-dots nav-small-cap-icon fs-4"></i>
     <span class="hide-menu">Perawatan</span>
 </li> --}}
 <li class="sidebar-item">
-    <a class="sidebar-link {{ request()->routeIs('tutorial-mesin.*') ? 'active' : '' }}"
-        href="{{ route('tutorial-mesin.index') }}"
-        aria-expanded="false">
+    <a class="sidebar-link {{ request()->routeIs('tutorial-mesin.*') ? 'active' : '' }}" href="{{ route('tutorial-mesin.index') }}" aria-expanded="false">
         <span>
             <i class="ti ti-video"></i>
         </span>
@@ -23,9 +21,7 @@
     </a>
 </li>
 <li class="sidebar-item">
-    <a class="sidebar-link {{ request()->routeIs('presensi.*') ? 'active' : '' }}"
-        href="{{ route('presensi.index') }}"
-        aria-expanded="false">
+    <a class="sidebar-link {{ request()->routeIs('presensi.*') ? 'active' : '' }}" href="{{ route('presensi.index') }}" aria-expanded="false">
         <span>
             <i class="ti ti-star"></i>
         </span>
@@ -33,9 +29,7 @@
     </a>
 </li>
 <li class="sidebar-item">
-    <a class="sidebar-link {{ request()->routeIs('perawatan.*') ? 'active' : '' }}"
-        href="{{ route('perawatan.index') }}"
-        aria-expanded="false">
+    <a class="sidebar-link {{ request()->routeIs('perawatan.*') ? 'active' : '' }}" href="{{ route('perawatan.index') }}" aria-expanded="false">
         <span>
             <i class="ti ti-article"></i>
         </span>
@@ -44,9 +38,7 @@
 </li>
 
 <li class="sidebar-item">
-    <a class="sidebar-link {{ request()->routeIs('operator-perbaikan.*') ? 'active' : '' }}"
-        href="{{ route('operator-perbaikan.index') }}"
-        aria-expanded="false">
+    <a class="sidebar-link {{ request()->routeIs('operator-perbaikan.*') ? 'active' : '' }}" href="{{ route('operator-perbaikan.index') }}" aria-expanded="false">
         <span>
             <i class="ti ti-article"></i>
         </span>
@@ -54,9 +46,7 @@
     </a>
 </li>
 <li class="sidebar-item">
-    <a class="sidebar-link {{ request()->routeIs('monitoring-suhu.*') ? 'active' : '' }}"
-        href="{{ route('monitoring-suhu.index') }}"
-        aria-expanded="false">
+    <a class="sidebar-link {{ request()->routeIs('monitoring-suhu.*') ? 'active' : '' }}" href="{{ route('monitoring-suhu.index') }}" aria-expanded="false">
         <span>
             <i class="ti ti-aperture"></i>
         </span>
