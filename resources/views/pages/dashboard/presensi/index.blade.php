b4b54a41219ac2c2608f0933b0a1bfd58978b3af
FeryPermana <muhammad.permana@students.amikom.ac.id>
2023-09-20 22:08:30 +0700

    as

--- resources/views/pages/dashboard/presensi/index.blade.php
+++ resources/views/pages/dashboard/presensi/index.blade.php
@@ -1,5 +1,5 @@
 <x-admin-layout>
-    <div class="card">
+    {{-- <div class="card">
         <div class="card-body">
             <div class="my-5">
                 <div class="row">
@@ -109,5 +109,15 @@
                     class="btn btn-primary">Simpan</button>
             </form>
         </div>
-    </div>
+    </div> --}}
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <a
+        href="https://docs.google.com/spreadsheets/d/1pwz3MbO9Hglcd_ug5EHsdvDBEsWYimhzMIMtXye_k5Y/edit#gid=715737916">excel</a><br>
+    <iframe width="1000"
+        height="1000"
+        src="https://docs.google.com/spreadsheets/d/e/2PACX-1vR5BPdIZnbpP7KVPt2VmCoiTtSAKKu-8OdBqLVM8XlH2amR29kSncaEMscgT_GvKFRN8Lz8r8lWT2tO/pubhtml?widget=true&amp;headers=false"></iframe>
 </x-admin-layout>
